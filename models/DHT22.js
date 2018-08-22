5ee09a8bb8dba70e4c3100abfee4ef46ea5608c0
flamezaxaou10 <flamezaxaou1@gmail.com>
2018-08-22 16:39:00 +0700

    dataformat

--- models/DHT22.js
+++ models/DHT22.js
@@ -19,35 +19,39 @@ var DHT22 = new mongoose.Schema({
     status: { type: String, default: 'disabled' },
     timestamp: { type: Date, default: Date.now }
   },
-  name: { type: String, default: 'Temperature and Humidity'},
-  description: { type: String, default: 'Current Temperature and Humidity'},
-  language: { type: String, default: 'en-US' },
-  sensorInfo: {
-    type: { type: String, default: 'Temperature'},
-    model: { type: String, default:'DHT22'}
-  },
-  categories: [
-    {
-      name: { type: String, default:'Temperature'}
+  metadata: {
+    thingId: { type: String, required: true },
+    type: { type: String, default: 'subsystem' },
+    name: { type: String, default: 'Temperature and Humidity' },
+    description: { type: String, default: 'Current Temperature and Humidity' },
+    language: { type: String, default: 'en-US' },
+    sensorInfo: {
+      type: { type: String, default: 'Temperature' },
+      model: { type: String, default: 'DHT22' }
     },
-    {
-      name: { type: String, default:'Humidity'}
-    }
-  ],
-  location: {
-    type: { type: String, default:'Fixed'},
-    address: { type: String },
-    city: { type: String },
-    county: { type: String },
-    country: { type: String },
-    postalCode: { type: String },
-    freeText: { type: String },
-    gps: {
-      latitude: { type: Number },
-      longitude: { type: Number }
+    categories: [
+      {
+        name: { type: String, default: 'Temperature' }
+      },
+      {
+        name: { type: String, default: 'Humidity' }
+      }
+    ],
+    location: {
+      type: { type: String, default: 'Fixed' },
+      address: { type: String },
+      city: { type: String },
+      county: { type: String },
+      country: { type: String },
+      postalCode: { type: String },
+      freeText: { type: String },
+      gps: {
+        latitude: { type: Number },
+        longitude: { type: Number }
+      }
     }
   },
-  version: { type: String, default: '1.0'}
+  version: { type: String, default: '1.0' }
 })
 
 module.exports = mongoose.model('thing', DHT22)
