addde508a423338fec4c5bda1ee011d551684165
flamezaxaou10 <flamezaxaou1@gmail.com>
2018-10-12 17:07:48 +0700

    ccc

--- models/Board.js
+++ models/Board.js
@@ -1,6 +1,7 @@
 var mongoose = require('mongoose')
 var Board = new mongoose.Schema({
-  boardName: { type: String, required: true }
+  boardName: { type: String, required: true },
+  colorName: {type: String, default: 'native'}
 })
 
 module.exports = mongoose.model('board', Board)
