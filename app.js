ed91f01bd9ccac1b75cbd4f7c9a3961d65e295fd
flamezaxaou10 <flamezaxaou1@gmail.com>
2018-11-09 09:23:33 +0700

    woohoo

--- app.js
+++ app.js
@@ -37,8 +37,8 @@ app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }))
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*")
   res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    "Access-Control-Allow-Headers", 
+    "Origin, X-Requested-With, Content-Type, Accept"
   )
   if (req.method === "OPTIONS") {
     res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET")
--- netpie.js
+++ netpie.js
@@ -47,7 +47,7 @@ microgear.on('message', function (topic, msg) {
         }
       }
     }).then(function (res) {
-      console.log(res)
+      //console.log(res)
     }).catch(function (error) {
       console.log(error)
     })
